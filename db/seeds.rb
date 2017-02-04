407fc993250bf7d989a0a30636e9cf3f2c6872d6
ryusei matsumoto <r9.life.is.art.21m@gmail.com>
2017-02-04 01:07:18 +0000

    アソシエーション

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+n = 1
+while n <= 100
+  Blog.create(
+    title: "あああ",
+    user_id: n
+  )
+  n = n + 1
+end
