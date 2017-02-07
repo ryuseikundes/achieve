702cc2c04b523344bfad2a276894a9be7021ac19
松本隆誠 <ryuseimatsumoto@matsumototakashimakoto-no-MacBook-Pro.local>
2017-02-07 13:38:11 +0900

    メール送信３

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-　config.action_mailer.default_url_options = { host: 'https://stormy-lake-97347.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://stormy-lake-97347.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
