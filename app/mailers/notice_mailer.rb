c39e8e0f379ab40cdac061f10c504d13ffbbedfc
松本隆誠 <ryuseimatsumoto@matsumototakashimakoto-no-MacBook-Pro.local>
2017-02-08 21:00:17 +0900

    mail

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -16,7 +16,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_contact(contact)
     @contact = contact
 
-    mail to: "Contact.email",
+    mail to: "contact.email",
       subject: 'お問い合わせが完了しました！'
   end
 end
