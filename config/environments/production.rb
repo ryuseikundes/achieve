cf9ca9e70f71f5a5ce341767c572af0ddaf319cc
松本隆誠 <ryuseimatsumoto@matsumototakashimakoto-no-MacBook-Pro.local>
2017-03-14 11:40:54 +0900

    production.rb、sendgridホスト名herokuからawsに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://stormy-lake-97347.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '52.198.186.239' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
