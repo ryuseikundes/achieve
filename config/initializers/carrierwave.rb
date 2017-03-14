a1ec5fcf3ec34fc96287e744548c4c343f7a5678
松本隆誠 <ryuseimatsumoto@matsumototakashimakoto-no-MacBook-Pro.local>
2017-03-14 12:38:21 +0900

    carrierwave内画像パス変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,9 +13,9 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = 'achieve-production0921'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-production0921'
     when 'development'
       config.fog_directory = 'achieve-development0921'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-development0921'
   end
 end
